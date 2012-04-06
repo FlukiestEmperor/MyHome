5f15d3bf51cb435c6c04d4a72b3ff87038fbe3fc
Spathi <spathi@playminecraft.net>
2012-04-06 17:27:12 +0100

    Vault instead of Register

--- src/main/java/me/taylorkelly/myhome/data/HomeEconomy.java
+++ src/main/java/me/taylorkelly/myhome/data/HomeEconomy.java
@@ -4,12 +4,14 @@ import me.taylorkelly.myhome.HomeSettings;
 import me.taylorkelly.myhome.utils.HomeLogger;
 
 import org.bukkit.plugin.Plugin;
-import com.nijikokun.register.Register;
-import com.nijikokun.register.payment.Method;
-import com.nijikokun.register.payment.Methods;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import net.milkbowl.vault.Vault;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 
 public class HomeEconomy {
-	private static Method economy;
+	private static Economy economy = null;
 	private static boolean econEnabled = false;
 	private static Plugin myhome;
 	private static String econname = null; 
@@ -18,63 +20,29 @@ public class HomeEconomy {
 	public static void init(Plugin plugin) {
 		HomeEconomy.myhome = plugin;
 		if(HomeSettings.eConomyEnabled) {
-			// Check if Register is already loaded
-			Register plug = (Register) myhome.getServer().getPluginManager().getPlugin("Register");
-			if (plug != null && plug.getClass().getName().equals("com.nijikokun.register.Register")) {
-				enableEconomy();
-				findEconomy();
-			}
-		}
-	}
-	
-	public static void findEconomy() {
-		if(!Methods.hasMethod()){
-			// Register doesnt know what to use.. Find something
-			HomeLogger.info("Finding an economy for Register.");
-			if(Methods.setMethod(myhome.getServer().getPluginManager())){
-				HomeEconomy.economy = Methods.getMethod();
-				econname = HomeEconomy.economy.getName();
-				econver = HomeEconomy.economy.getVersion();
-				HomeLogger.info("Connected to " + HomeEconomy.economy.getName() + " v" + HomeEconomy.economy.getVersion() + " for economy support.");
-			} else {
-				// Register found nothing it can use. Abort abort abort.
-				noCompatibleEcon();
-			}
-		} else if(Methods.hasMethod()) {
-			// Register already knows what to use
-			HomeEconomy.economy = Methods.getMethod();
-			econname = HomeEconomy.economy.getName();
-			econver = HomeEconomy.economy.getVersion();
-			HomeLogger.info("Connected to " + HomeEconomy.economy.getName() + " v" + HomeEconomy.economy.getVersion() + " for economy support.");
-		} else {
-			// Register found nothing it can use. Abort abort abort.
-			noCompatibleEcon();
-		}
-	}
-	
-	public static void noCompatibleEcon() {
-		disableEconomy();
-		HomeSettings.eConomyEnabled = false;
-		HomeLogger.warning("===============================================");
-		HomeLogger.warning("Economy support is enabled but Register was unable to find");
-		HomeLogger.warning("a compatible economy plugin. See the following url for a list of");
-		HomeLogger.warning("compatible plugins: http://forums.bukkit.org/threads/16849/");
-		HomeLogger.warning("===============================================");
-
-	}
-	
-	public static void hasUnhooked(Plugin deadplugin) {
-		if (economy != null && Methods.hasMethod()) {
-			Boolean check = Methods.checkDisabled(deadplugin);
-			if(check) {
-				economy = null;
-				HomeLogger.info("Payment method: " + econname + " v" + econver + " was disabled. Suspending economy services.");
+	        if (myhome.getServer().getPluginManager().getPlugin("Vault") == null) {
+				HomeLogger.warning("===============================================");
+				HomeLogger.warning("Economy support is enabled in config but Vault was not found. Is it installed?");
+				HomeLogger.warning("===============================================");
 				disableEconomy();
 				HomeSettings.eConomyEnabled = false;
-			}
+				return;
+	        }
+	        RegisteredServiceProvider<Economy> rsp = myhome.getServer().getServicesManager().getRegistration(Economy.class);
+	        if (rsp == null) {
+	        	HomeLogger.warning("===============================================");
+				HomeLogger.warning("Vault could not hook into an economy plugin.");
+				HomeLogger.warning("===============================================");
+				disableEconomy();
+				HomeSettings.eConomyEnabled = false;
+				return;
+	        }
+	        economy = rsp.getProvider();
+			enableEconomy();
+			HomeLogger.info("Economy: Using Vault for economy services");
 		}
 	}
-
+	
 	public static void disableEconomy() {
 		econEnabled = false;
 	}
@@ -89,13 +57,7 @@ public class HomeEconomy {
 	
 	public static boolean economyReady() {
 		if(!econEnabled && HomeSettings.eConomyEnabled == true) {
-			// Suspend future attempts to call economy stuff.
 			HomeSettings.eConomyEnabled = false;
-			HomeLogger.warning("===============================================");
-			HomeLogger.warning("Economy support is enabled in config but Register was not found. Is it installed?");
-			HomeLogger.warning("Economy support has been suspended. MyHome will not charge users until Register is installed.");
-			HomeLogger.warning("To download Register see: http://forums.bukkit.org/threads/16849/");
-			HomeLogger.warning("===============================================");
 			return false;
 		} else {
 			return true;
@@ -114,9 +76,9 @@ public class HomeEconomy {
 		if(!economyReady()) { return false; }
 		
 		if(hasAccount(name)) {
-			if(hasEnough(name, amount)) { 
-				economy.getAccount(name).subtract(amount);
-				return true;
+			if(hasEnough(name, amount)) {
+				EconomyResponse resp = economy.withdrawPlayer(name,amount);
+				return resp.transactionSuccess();
 			} else {
 				return false;
 			}
@@ -129,13 +91,13 @@ public class HomeEconomy {
 	public static boolean hasEnough(String name, float amount) {
 		if(!economyReady()) { return false; }
 		
-		return economy.getAccount(name).hasEnough(amount);
+		return economy.has(name, amount);
 	}
 
 	public static double balance(String name){
 		if(!economyReady()) { return 0; }
 		
-		return economy.getAccount(name).balance();
+		return economy.getBalance(name);
 	}
 
 	public static String formattedBalance(double amount){
